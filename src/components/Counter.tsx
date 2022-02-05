c9eb6814032e738ef803dd4bef24568ebd43b159
adamrturman <adamrturman@gmail.com>
2022-02-05 12:27:08 -0600

    Redux working with nulls approach

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,57 +1,56 @@
 import React, {Dispatch} from 'react';
 import {connect} from 'react-redux';
 import {Action} from "redux";
-import { isNumber } from "../helpers/isNumber";
 import { generateRandomInteger } from "../helpers/generateRandomInteger";
 
 interface AsyncDivisionProps {
     numerator: number;
     denominator: number;
-    numberOfNumbersSet: number;
     randomlyIncrementNumerator: () => void;
     randomlyIncrementDenominator: () => void;
     resetBoth: () => void;
 }
 
-const AsyncDivision = ({numerator, denominator, numberOfNumbersSet, randomlyIncrementNumerator, randomlyIncrementDenominator, resetBoth}: AsyncDivisionProps) => {
-    function loadSingleNumber(callback: (number: number) => void) {
+const AsyncDivision = ({numerator, denominator, randomlyIncrementNumerator, randomlyIncrementDenominator, resetBoth}: AsyncDivisionProps) => {
+    function loadSingleNumber(callback: (number: number) => void, extraDelay: number) {
         const randomInteger = generateRandomInteger();
 
         setTimeout(() => {
-            // numberOfNumbersSet++;
             callback(randomInteger)
-        }, randomInteger * 500)
+        }, (randomInteger + extraDelay) * 500)
     }
 
     const handleIncrement = () => {
-        loadSingleNumber(randomlyIncrementDenominator);
-        loadSingleNumber(randomlyIncrementNumerator);
+        loadSingleNumber(randomlyIncrementDenominator, 0);
+        loadSingleNumber(randomlyIncrementNumerator, 3);
     }
 
-    const answer = numerator / denominator;
+    let numberDisplay: string | number = 'loading';
+
+    if (numerator !== null && denominator !== null) {
+        numberDisplay = numerator / denominator;
+    }
+
     return (
       <>
         <div>Current numerator: {numerator}</div>
         <div>Current denominator: {denominator}</div>
-        <div>Current Async Division: {numberOfNumbersSet < 2 ? 'Loading' : answer} </div>
-          <button onClick={handleIncrement}>Randomly increment</button>
+        <div>Current Async Division: {numberDisplay} </div>
+        <button onClick={handleIncrement}>Randomly increment</button>
         <button onClick={resetBoth}>Reset both</button>
       </>
     );
 }
 
-
 interface StoreState {
     numerator: number
     denominator: number
-    numberOfNumbersSet: number
 }
 
 function mapStateToProps(state: StoreState) {
     return {
         numerator: state.numerator,
-        denominator: state.denominator,
-        numberOfNumbersSet: state.numberOfNumbersSet
+        denominator: state.denominator
     };
 }
 
--- src/reducers/numbersSetReducer.ts
+++ src/reducers/numbersSetReducer.ts
@@ -1,14 +1,27 @@
 import {Action} from "redux";
 import { generateRandomInteger } from "../helpers/generateRandomInteger";
 
-const initialState = { numerator: 0, denominator: 0, numberOfNumbersSet: 0};
+interface StoreState {
+  numerator: number | null;
+  denominator: number | null;
+}
+
+const initialState: StoreState = { numerator: null, denominator: null};
 
 export const numbersSetReducer = (state = initialState, action: Action<string>) => {
   switch (action.type) {
     case 'INCREMENT_NUMERATOR':
-      return {...state, numerator: state.numerator + generateRandomInteger(), numberOfNumbersSet: state.numberOfNumbersSet + 1};
+      if(state.numerator === null) {
+        return {...state, numerator: generateRandomInteger()};
+      }
+
+      return {...state, numerator: state.numerator + generateRandomInteger()};
     case 'INCREMENT_DENOMINATOR':
-      return {...state, denominator: state.denominator + generateRandomInteger(), numberOfNumbersSet: state.numberOfNumbersSet + 1};
+      if(state.denominator === null) {
+        return {...state, denominator: generateRandomInteger()};
+      }
+
+      return {...state, denominator: state.denominator + generateRandomInteger()};
     case 'RESET_BOTH' :
       return initialState;
     default:
